28fdfb43489473367ef5d35e384c6938f68839f6
vangogh500 <kai.matsuda@hamu.design>
2021-07-17 05:39:27 -0400

    SKIN-38 rename cluster id

--- modules/data-stores/sql/main.tf
+++ modules/data-stores/sql/main.tf
@@ -1,12 +1,12 @@
 resource "aws_rds_cluster" "skincare" {
-  cluster_identifier = "aurora-cluster-demo"
+  cluster_identifier = "skincare"
   engine = "aurora-mysql"
-  engine_version  = "5.7.serverless_mysql_aurora.2.07.1"
+  engine_version  = "5.7.mysql_aurora.2.07.1"
   database_name = "skincare"
   master_username = "${var.master_username}"
   master_password = "${var.master_password}"
   engine_mode = "serverless"
-  backup_retention_period = 0
+  backup_retention_period = 1
   skip_final_snapshot = true
   scaling_configuration {
     auto_pause               = true
